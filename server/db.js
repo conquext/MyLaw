3fc01284e564f1319eb0479e2e92ee94d9d3fa01
Farce <a.rasheedalabi@gmail.com>
2019-12-08 23:55:31 +0100

    initialize seed data

---
+++ server/db.js
@@ -0,0 +1,31 @@
+export const attendees = [
+  {
+    id: 1,
+    name: "Yurinaya Olotoni",
+    email: "email1@email.com"
+  },
+  {
+    id: 2,
+    name: "Alata Sinensis",
+    email: "email2@email.com"
+  }
+];
+
+export const talks = [
+  {
+    id: 1,
+    topic: "Immersiveness of design thinking in hyperbolic metamorphism",
+    speakerId: "1"
+  },
+  {
+    id: 2,
+    topic: "Joggernault Minimalist eccentricity in cacophonic deconstruction",
+    speakerId: "2"
+  },
+  {
+    id: 3,
+    topic: "Deletedness of deleted properties in public disclosure",
+    speakerId: "1",
+    deleted: true
+  }
+];
