785d6fdf605c3a4195525a610bf79008c7cd5778
Farce <intbusfor@gmail.com>
2019-12-15 17:39:13 +0100

    serve build for backend

--- server/build/app.js
+++ server/build/app.js
@@ -12,6 +12,8 @@ var _cors = _interopRequireDefault(require("cors"));
 
 var _index = _interopRequireDefault(require("./routes/index"));
 
+var _path = _interopRequireDefault(require("path"));
+
 _dotenv["default"].config();
 
 var app = (0, _express["default"])();
@@ -26,9 +28,9 @@ app.use(function (req, res, next) {
   next();
 });
 (0, _index["default"])(app);
-console.log("dir", __dirname + "../../client/dist");
-app.use(_express["default"]["static"](__dirname + "../../client/dist"));
-app.get("/*", function (req, res) {//   res.sendFile(path.join(__dirname, "index.html"));
+app.use(_express["default"]["static"](_path["default"].resolve(__dirname, "../../client/public")));
+app.get("/*", function (req, res) {
+  res.sendFile(_path["default"].resolve(__dirname, "../../client/public/index.html"));
 });
 var PORT = process.env.PORT || 2000;
 app.listen(PORT, console.info("server started on port ".concat(PORT)));
